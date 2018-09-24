5971ca765736ca94d30db728fc0b157bf71b11d9
akhilaadkoli <aadkol2@illinois.edu>
2018-09-24 15:46:29 -0500

    version 1

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class TimesTable {
+    public static int[][] generateTimesTable(int first, int second) {
+
+    }
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
